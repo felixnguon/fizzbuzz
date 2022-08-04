14c335c70a9b790541c27ed96e0cf117f1ee3496
Felix Nguon <55321150+nguonduc@users.noreply.github.com>
2022-08-04 11:00:07 +0700

    feat: implement fizzbuzz

---
+++ index.js
@@ -0,0 +1,26 @@
+/*
+ * Check number and conditionally return result
+ * if the number is a multiply of 3, return Fizz
+ * if the number is a multiply of 5, return Buzz
+ * if the number is a multiply of 3 & 5, return FizzBuzz
+ * @params i number
+ * @return output string | number
+ */
+function fizzbuzz(i) {
+  let output = ''
+  if (i % 3 == 0) output += 'Fizz'
+  if (i % 5 == 0) output += 'Buzz'
+  if (output == '') output = i
+  return output
+}
+
+/*
+ * Check numbers from 1 to 100, print the desired output
+ */
+function printFizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    console.log(fizzbuzz(i))
+  }
+}
+
+printFizzBuzz()
